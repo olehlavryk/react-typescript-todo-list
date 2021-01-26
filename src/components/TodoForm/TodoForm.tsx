a84a14b235a068c4c22c4cc772efb160a6dae64e
Oleh Lavryk <oleh.lavrik@gmail.com>
2021-01-26 18:10:06 +0200

    Add NavBar and Todoform components

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -2,17 +2,11 @@ import React, { useRef } from "react";
 
 export const TodoForm: React.FC = () => {
   const ref = useRef<HTMLInputElement>(null);
-  //   const [title, setTitle] = useState<string>("");
-
-  //   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //     setTitle(event.target.value);
-  //   };
 
   const keyPressHandler = (event: React.KeyboardEvent) => {
     if (event.key === "Enter") {
       console.log(ref.current!.value);
       ref.current!.value = "";
-      //setTitle(event.target.);
     }
   };
 
@@ -23,13 +17,11 @@ export const TodoForm: React.FC = () => {
       </label>
       <input
         ref={ref}
-        // onChange={changeHandler}
         onKeyPress={keyPressHandler}
         type="text"
         className="text"
         id="title"
         placeholder="Enter todo task"
-        // value={title}
       />
     </div>
   );
